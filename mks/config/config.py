2695eca2cfc19918a1fa1b7ca19c0fe476b4b43e
vpsbotnew <121183724+vpsbotnew@users.noreply.github.com>
2023-03-08 13:00:09 +0630

    Update config.py

--- mks/config/config.py
+++ mks/config/config.py
@@ -84,13 +84,13 @@ class Config:
 
 ⭐️Lifetime ၃၀၀၀ ကျပ်နဲ့ မန်ဘာဝင်ပြီးကြည့်နိုင်ပါတယ်။
 
-<a href='https://t.me/YNVIPMEMBERBOT'>💫အသေးစိတ်ကြည့်ရန် ဤနေရာကိုနှိပ်ပါ💫</a>
+<a href='https://t.me/YNVIPMEMBERBOT'>💫အသေးစိတ်ကြည့်ရန် ဤနေရာကိုနှိပ်ပါ</a>
 
-<a href='https://t.me/YN_VIP_Series_ListAndPoster'>🟢တင်ပြီးသားစီးရီးအမည်ကြည့်ရန် ဤနေရာကိုနှိပ်ပါ🟢</a>
+<a href='https://t.me/YN_VIP_Series_ListAndPoster'>🟢စီးရီးအမည်ကြည့်ရန် ဤနေရာကိုနှိပ်ပါ</a>
 
-<a href='https://t.me/YoeNaungAllMovieList'>🔴တင်ပြီးသားစီးရီးအညွှန်းဖတ်ရန် ဤနေရာကိုနှိပ်ပါ🔴</a>
+<a href='https://t.me/YoeNaungAllMovieList'>🔴စီးရီးအညွှန်းများဖတ်ရန် ဤနေရာကိုနှိပ်ပါ</a>
 
-<a href='https://t.me/YoeNaung'>😘Adminနဲ့ဆက်သွယ်ရန် ဤနေရာကိုနှိပ်ပါ</a>\n\n 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 [『..Dr Yoe..』](t.me/YoeNaung)""")
+<a href='https://t.me/YoeNaung'>😘Adminနဲ့ပြောရန် ဤနေရာကိုနှိပ်ပါ</a>\n\n 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 [『..Dr Yoe..』](t.me/YoeNaung)""")
     
     LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
     SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "False"), False)
